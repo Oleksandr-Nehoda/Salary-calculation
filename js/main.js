c77628d72a44b48d0523425b6cae6a1f01043be0
Oleksandr Nehoda <iamsoul@ukr.net>
2024-01-19 17:59:53 +0200

    Add percent logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-const refInp = document.querySelector(".all-money");
-const refButton = document.querySelector(".calc-total");
-const refSpan = document.querySelector(".total-num")
+const refInp = document.querySelector('.all-money');
+const refButSum = document.querySelector('.calc-total');
+const refSpan = document.querySelector('.total-num');
+const refButClean = document.querySelector('.clean');
+const refRadioBtn30 = document.querySelector('input[value="30"]')
+const refRadioBtn35 = document.querySelector('input[value="35"]')
 let inpValue = '';
 let sum = 0;
+let percent = 0;
+console.log(`sum - ${sum}`)
 
-const handleClick = () => {
+
+const handleClickSum = () => {
     const array = inpValue.split(" ");
     const newArray = array.map((number) => {
         if(number.length === 0) {
@@ -26,11 +32,36 @@ const handleClick = () => {
     refSpan.textContent = sum;
 }
 
-const handleInput = (event) => {
+const handleInpSum = (event) => {
     inpValue = event.currentTarget.value
 }
 
-refButton.addEventListener('click', handleClick)
-refInp.addEventListener('input', handleInput)
+const handleClickClean = () => {
+    refInp.value = "";
+    refSpan.textContent = 0;
+    refRadioBtn30.checked = false;
+    refRadioBtn35.checked = false;
+    sum = 0;
+    inpValue = '';
+    
+}
+
+const handleChangePercent = (event) => {
+
+    if (event.currentTarget.checked && event.currentTarget.value === '30') {
+        percent = Math.ceil(sum * 0.3);
+        refSpan.textContent = percent;
+    }
+    if (event.currentTarget.checked && event.currentTarget.value === '35') {
+        percent = Math.ceil(sum * 0.35);
+        refSpan.textContent = percent;
+    }
+}
+
+refButSum.addEventListener('click', handleClickSum)
+refInp.addEventListener('input', handleInpSum)
+refButClean.addEventListener('click', handleClickClean)
+refRadioBtn30.addEventListener('change', handleChangePercent)
+refRadioBtn35.addEventListener('change', handleChangePercent)
 
 
